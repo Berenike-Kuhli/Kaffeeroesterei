6fa23211a4a82f3441f0e38447e04c8203ba49de
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-02-15 23:07:14 +0100

    Menü mit JS klappbar

---
+++ src/js/openMenu.js
@@ -0,0 +1,10 @@
+// basiert auf w3 tutorial
+// eslint-disable-next-line no-unused-vars
+function openMenu() {
+  const x = document.getElementById("subpagelinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
